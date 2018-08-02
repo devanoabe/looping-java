1d8ac3aa6a382d1c39c50e28e88bee300d558d12
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-02 07:47:10 +0700

    For Whie Do-While

---
+++ src/j08/Contoh1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j08;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Contoh1 {
+    public static void main (String[] args) 
+    {
+        
+        //PERULANGAN FOR
+        int x;
+        for(x = 1; x <=5; x++)
+        {
+            System.out.println("SMK TELKOM MALANG");
+        }
+        
+        //PERULANGAN WHILE
+        int i = 1;
+        while(i <=5 )
+        {
+            System.out.println("SMK TELKOM MALANG");
+            i++;
+        }
+        
+        //PEULANGAN DO WHILE
+        int j = 1;
+        do
+        {
+            System.out.println("SMK TELKOM MALANG");
+            j++;
+        }
+        while(j <= 5);
+    
+    
+    }
+    
